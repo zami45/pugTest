1919d18da95b9904d978b3fa156e8d9d70bdecae
zami <alzami.shimanta@gmail.com>
2019-03-02 21:50:31 +0600

    code refactoring

--- testpug.js
+++ testpug.js
@@ -1,4 +1,4 @@
-//import necessary libraries
+//import necessary libraries & dataset
 const pug = require("pug");
 const fs = require("fs");
 const readline = require("readline").createInterface({
@@ -10,24 +10,22 @@ const database = require("./database.json");
 //grab the base template
 const template = "./pugTemplate/sports.pug";
 
-//delete existing html file if there is anys
+//delete existing html file from Resource directory if there is anys
 fs.readdir("./Resource", (error, files) => {
   if (error) throw error;
   files
-    .filter(name => /.*?\.html$/.test(name))
-    .forEach(e => fs.unlinkSync("./Resource/" + e));
+    .filter(fname => /.*?\.html$/.test(fname))
+    .forEach(filename => fs.unlinkSync("./Resource/" + filename));
 });
 
-//get imaginary url name from user
+//get imaginary url name from user in order to generate .html file
 readline.question(`What's your url?`, name => {
-
   let dest = `./Resource/${name}.html`;
   let resourceObj = { sports: database[name] };
 
   let compiledFunction = pug.compileFile(template);
   fs.writeFile(dest, compiledFunction(resourceObj), "utf-8", () =>
-    console.log("success")
+    console.log("successfully generated")
   );
   readline.close();
-
 });
