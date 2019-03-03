02229bb71bf6e646dfc8473fd8750145d21c6ef0
shimantta <alzami.shimanta@gmail.com>
2019-03-03 17:08:17 +0600

    full functional webpages which are linked

--- testpug.js
+++ testpug.js
@@ -1,31 +1,31 @@
-//import necessary libraries & dataset
+//import necessary libraries, dataset & base-template
 const pug = require("pug");
 const fs = require("fs");
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 const database = require("./database.json");
-
-//grab the base template
-const template = "./Layout/layout.pug";
+const template = "./pageTemplate/sportsTemplate.pug";
+const htmlFileNames = database["htmlFileNames"];
 
 //delete existing html file from Resource directory if there is anys
-fs.readdir("./Resource", (error, files) => {
+fs.readdirSync("./Resource", (error, files) => {
   if (error) throw error;
   files
     .filter(fname => /.*?\.html$/.test(fname))
-    .forEach(filename => fs.unlinkSync("./Resource/" + filename));
+    .forEach(filename =>
+      fs.unlinkSync("./Resource/" + filename, () => {
+        console.log(filename);
+      })
+    );
 });
 
-//get imaginary url name from user in order to generate .html file
-readline.question(`What's your url?`, name => {
-  let dest = `./Resource/${name}.html`;
-  let resourceObj = { sports: database[name] };
+// //get imaginary url name from user in order to generate .html file
+
+htmlFileNames.forEach(fileObj => {
+  let dest = `./Resource/${fileObj.fname}`;
+  let template = `./pageTemplate/${fileObj.template}`;
+  let resourceObj = { page : database[fileObj.fname] };
 
   let compiledFunction = pug.compileFile(template);
   fs.writeFile(dest, compiledFunction(resourceObj), "utf-8", () =>
     console.log("successfully generated")
   );
-  readline.close();
 });
