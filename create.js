290e43aafee1b8bba2dd77b51e00bbdabd5468c9
zami <alzami.shimanta@gmail.com>
2019-03-08 21:22:12 +0600

    new html generation from pug template string

--- create.js
+++ create.js
@@ -12,13 +12,28 @@ traverseMenuItemsRecursive(htmlFileNames);
 
 //to generate .html file
 function generateHtml(eachMenu) {
-  let dest = `./Resource/${eachMenu.link}`;
-  let linkDefinition = { page: database["link_definitions"][eachMenu.label] };
-  linkDefinition.page.commonHeader = database["link_definitions"]["commonHeader"];
-  let compiledFunction = pug.compileFile(linkDefinition.page.template);
-  fs.writeFile(dest, compiledFunction(linkDefinition), "utf-8", () =>
+  const dest = `./Resource/${eachMenu.link}`
+  const link_definition = database.link_definitions[eachMenu.link]
+  const content = { page: link_definition }
+  content.page.commonHeader = database.link_definitions.commonHeader
+  const templatePath = database.skeletons[link_definition.skeleton].path
+  const pugheader = templatePath + "/header.pug"
+  const pugfooter = templatePath + "/footer.pug"
+  const pugpagetemplate = templatePath+"/" + link_definition.template
+  const pugSkeleton = generatePugString({ header : pugheader , footer : pugfooter, page : pugpagetemplate })
+
+  const compileFn = pug.compile(pugSkeleton, {
+    filename: "pug",
+    pretty: true
+  });
+  
+  fs.writeFile(dest, compileFn(content), "utf-8", () =>
     console.log(eachMenu.link + "-successfully generated.")
   );
+}
+
+function generatePugString(obj){
+  return `doctype html\ninclude ${obj.header}\ninclude ${obj.page}\ninclude ${obj.footer}`
 }
 
 function traverseMenuItemsRecursive(menuItems = []) {
@@ -35,11 +50,11 @@ function traverseMenuItemsRecursive(menuItems = []) {
 }
 
 function deleteAllHtmlFile() {
-  try{
+  try {
     fs.readdirSync("./Resource")
-    .filter(item => /.*?\.html$/.test(item))
-    .forEach(htmlfile => fs.unlinkSync("./Resource/" + htmlfile));
-  }catch(e){
-    console.log(e)
+      .filter(item => /.*?\.html$/.test(item))
+      .forEach(htmlfile => fs.unlinkSync("./Resource/" + htmlfile));
+  } catch (e) {
+    console.log(e);
   }
 }
